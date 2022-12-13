67a72d9c1e77cc35165e474a24978ad8c5f16417
Bravetomato <princeps2nd@gmail.com>
2022-12-13 20:45:38 +0900

    stopwatch-3

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -3,13 +3,18 @@ import React, { useState } from "react";
 function StopWatch() {
   const [num, setNum] = useState(0);
 
-  const timeoutId = setTimeout(
+  let timeoutId = setTimeout(
     () => setNum(num + 1),
     1000
-  ); /*리렌더링 되면서 1초에 숫자가 1씩 추가된다*/
+  ); /*리렌더링 되면서 1초에 숫자가 1씩 추가된다. let으로 선언한 것이 포인트*/
 
   const pause = () => clearTimeout(timeoutId);
-  // 실행할 코드가 하나 뿐이면 {} 안써도 된다
+  // 실행할 코드가 하나 뿐이면 {} 안써도 된다. 카운터 일시정지 함수.
+
+  const resome = () => {
+    timeoutId = setTimeout(() => setNum(num + 1), 1000);
+  };
+  // 카운터를 재개시키는 함수. let timeoutId 과 15번 줄의 timeoutId 는 같은 것.
 
   return (
     <>
@@ -18,6 +23,9 @@ function StopWatch() {
       {/* 한줄 삽입 */}
       <button onClick={pause}>일시정지</button>
       {/* 버튼을 누르면 리렌더링이 멈추면서 숫자 추가가 멈추게 된다 */}
+      <hr />
+      <button onClick={resome}>재개</button>
+      {/* 버튼을 누르면 resome함수를 시작해 다시 카운터가 재개된다 */}
     </>
   );
 }
