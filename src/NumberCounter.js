83377949dc3a6518c8ff5de388df83f6b8134376
Bravetomato <princeps2nd@gmail.com>
2022-12-13 21:40:36 +0900

    ||연산자

--- src/NumberCounter.js
+++ src/NumberCounter.js
@@ -5,17 +5,32 @@ function NumberCounter() {
 
   // 숫자가 짝수일 때 짝수입니다. 문구 등장하도록 만들기
 
-  const noIsEvenDiv =
-    no % 2 == 0 ? (
-      <>
-        <span>짝수입니다.</span>
-        <hr /> {/* 참일 때 실행 */}
-      </>
-    ) : (
-      <></>
-      // 거짓일 때 실행
-    );
+  // const noIsEvenDiv =
+  //   no % 2 == 0 ? (
+  //     <>
+  //       <span>짝수입니다.</span>
+  //       <hr /> {/* 참일 때 실행 */}
+  //     </>
+  //   ) : (
+  //     <></>
+  //     // 거짓일 때 실행
+  //   );
 
+  // 삼항연산자의 다른 표현 -2
+  const noIsEvenDiv = (
+    <>
+      {no % 2 == 0 ? <div>짝수입니다.</div> : <div>홀수입니다.</div>}
+      <hr />
+    </>
+  );
+
+  // || 연산자 사용. no가 8로 나누어 몫이 0이거나
+  const noIsNot8MultipleDiv = no % 8 == 0 || (
+    <>
+      <div>8의 배수가 아닙니다.</div>
+      <hr />
+    </>
+  );
   // const noIs8MultipleDiv =
   //   no % 8 == 0 ? (
   //     <>
@@ -25,7 +40,7 @@ function NumberCounter() {
   //     <></>
   //   );
 
-  //위 삼항연산자를 이런식으로도 표현할 수 있다.
+  //위 삼항연산자를 이런식으로도 표현할 수 있다 -1
   const noIs8MultipleDiv = no % 8 == 0 && (
     <>
       <span>8의 배수입니다.</span>
@@ -40,6 +55,7 @@ function NumberCounter() {
       <hr />
       {noIsEvenDiv}
       {noIs8MultipleDiv}
+      {noIsNot8MultipleDiv}
     </>
   );
 }
