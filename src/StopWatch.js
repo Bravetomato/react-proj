cec7371ae20631c9d3749b468f6674fe814e0121
Bravetomato <princeps2nd@gmail.com>
2022-12-13 19:49:44 +0900

    stopwatch-2

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function StopWatch() {
   const [num, setNum] = useState(0);
 
-  setTimeout(() => setNum(num + 1), 1000);
+  setTimeout(() => setNum(num + 1), 1000); /*1초에 숫자가 1씩 추가된다*/
 
   return <>숫자 : {num}</>;
 }
