1a9f040656715e0ffd1dc38feab44dc4a18bea83
Bravetomato <princeps2nd@gmail.com>
2022-12-15 20:43:20 +0900

    팝업버트하나로

--- src/Popup.js
+++ src/Popup.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 
-// 팝업버튼 누르면 사각형 사라지도록-조건문
+// 팝업버튼 누르면 사각형 사라지도록-버튼 하나
 
 function Popup() {
   const border = "10px solid red";
   const [popupVisible, setPopupVisible] = useState(true);
+  // 기본값 popupVisible, 변경값 setPopupVisible
 
   return (
     <>
-      {popupVisible && <button className="btn btn-outline" onClick={() => setPopupVisible(false)}>팝업닫기</button>}
-      {/*  && 앞 조건이 참일 때*/}
-
-      {popupVisible || <button className="btn btn-outline" onClick={() => setPopupVisible(true)}>팝업열기</button>}
-      {/* || 앞 조건이 거짓 일 때   */}
-
+      <button className="btn btn-outline" onClick={() => setPopupVisible(!popupVisible)}>
+        팝업{popupVisible ? "닫기" : "열기"}
+        {/* popupVisible ture면 팝업닫기, popupVisible false면 팝업열기 */}
+        </button>
       <hr />
+
       {popupVisible && <div style={{ width: 100, height: 100, border }}></div>}
+      {/* popupVisible이 true면 사각형 나타남 */}
     </>
   );
 }
