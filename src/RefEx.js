29dc258162fcff6871e007a9ed16bdb8b634a511
Bravetomato <princeps2nd@gmail.com>
2022-12-22 20:14:21 +0900

    useRef-2

--- src/RefEx.js
+++ src/RefEx.js
@@ -1,37 +1,23 @@
 import React, { useState, useRef } from "react";
 
+
+let RefCallCount = 0;
+
 function RefEx() {
-  const formInputNoRef = useRef(null);
-  // Ref 만드는 법
-  const[no, setNo] = useState("");
- 
-  const notice = () => {
-    formInputNoRef.current.focus();
-    // 함수이름.current.기능() : 실행버튼 누른 후 커서 입력창으로 자동 이동
+  RefCallCount++;
+  console.log(`RefExrk ${RefCallCount}번 실행됨!`);
 
-    if (!no) { // 입력된 값이 숫자가 아니라면
-      alert("숫자를 입력해주세요.");
-      return;
-    }
-
-    alert(`당신이 입력한 숫자는 ${no}입니다.`);
-    setNo("");
-    // 입력한 숫자 지우기
-  };
+  const [no1, setNo1] = useState(0);
+  const [no2, setNo2] = useState(0);
+  const no3Ref = useRef(0);
 
   return (
     <>
-      <form onSubmit={(e) => {e.preventDefault(); notice();}}>
-        <input 
-        ref={formInputNoRef}
-        // 특정 el에 Ref에 심을 수 있다. 원래 있던 태그에만 심을 수 있다. 
-        type="text" 
-        placeholder="숫자" 
-        value={no} 
-        onChange={(e) => setNo(e.target.value)}
-        />
-        <button>실행</button>
-      </form>
+      <button onClick={() => setNo1(no1 + 1)}>숫자 1 증가 : {no1}</button>
+      <br />
+      <button onClick={() => setNo2(no2 + 2)}>숫자 2 증가 : {no2}</button>
+      <br />
+      <button onClick={() => no3Ref.current++}>숫자 3 증가 : {no3Ref.current}</button>
     </>
   );
 }
