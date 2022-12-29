fd0e12192ab3ecc2584d0ac704b7b545c0d3aef2
Bravetomato <princeps2nd@gmail.com>
2022-12-29 20:11:14 +0900

    부모-자식 컴포넌트

--- src/App.js
+++ src/App.js
@@ -3,36 +3,35 @@ import React, { useState, useEffect, useRef } from "react";
 
 import "./App.css";
 
+let SubCallCount = 0;
 let AppCallCount = 0;
 
-function App() {
-  // useEffect(() => {
-  //   AppCallCount++;
-  //   console.log(`App이 ${AppCallCount}번 실행됨.`);
-  // }, []);
-  // [] : 한번만 실행 }); : 계속 실행.
-  const inputAgeRef = useRef(null);
-  const inputNameRef = useRef(null);
+function Sub() {
+  SubCallCount++;
+  console.log(`Sub가 ${SubCallCount}번 실행!`);
   const [no, setNo] = useState(0);
-
-  // useEffect를 통해 이름입력창에 커서 자동으로 설정하기.
-  // setTimeout(() => inputNumberRef.current.focus(), 100); 아래 useEffect와 같은 기능 작동.
-  useEffect(() => {
-    inputNameRef.current.focus();
-  });
 
   return (
     <>
-      <input ref={inputNameRef} type="text" placeholder="이름" />
-      <input ref={inputAgeRef} type="number" placeholder="나이" />
-      <button
-        onClick={() => {
-          setNo(no + 1);
-          inputAgeRef.current.focus();
-        }}
-      >
-        증가 : {no}
-      </button>
+      <div style={{ border: "10px solid blue" }}>
+        <button onClick={() => setNo(no + 1)}>증가 : {no}</button>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  AppCallCount++;
+  console.log(`App이 ${AppCallCount}번 실행!`);
+  const [no, setNo] = useState(0);
+
+  return (
+    <>
+      <div style={{ border: "10px solid red" }}>
+        <button onClick={() => setNo(no + 1)}>증가 : {no}</button>
+        <hr />
+        <Sub />
+      </div>
     </>
   );
 }
