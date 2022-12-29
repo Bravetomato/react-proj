5122d5d1e72644a92cc7f79b60245a2d266deaba
Bravetomato <princeps2nd@gmail.com>
2022-12-29 21:16:56 +0900

    제이쿼리 다크모드 코드를 리액트 문법으로 바꾸기

--- src/App.js
+++ src/App.js
@@ -1,63 +1,40 @@
-import Notice from "./Notice";
 import React, { useState, useEffect, useRef } from "react";
 
 import "./App.css";
-// App : 부모컴포넌트 Sub : 자식컴포넌트
-// 부모가 리렌더링되면 자식도 리렌더링된다
-// 그러나 자식이 리렌더링 되었을 때 부모는 리렌더링되지 않음.
-
-let SubCallCount = 0;
-let AppCallCount = 0;
-
-function Sub({ appNo }) {
-  SubCallCount++;
-  console.log(`Sub가 ${SubCallCount}번 실행!`);
-  const [no, setNo] = useState(0);
-
-  // useEffect, 의존성 배열 인자 실험
-  useEffect(() => {
-    console.log("effect 1 : 단 한 번 실행");
-  }, []);
-
-  useEffect(() => {
-    console.log("effect 2 : 부모(App)의 appNo가 바뀔 때 마다 실행");
-  }, [appNo]);
-
-  useEffect(() => {
-    console.log("effect 3 : 나(Sub)의 no가 바뀔 때 마다 실행");
-  }, [no]);
-
-  useEffect(() => {
-    console.log("effect 4 : appNo 또는 no가 변경될 때 마다 실행");
-  }, [appNo, no]);
-
-  useEffect(() => {
-    console.log("effect 5 : 매번 실행");
-  });
-
-  return (
-    <>
-      <div style={{ border: "10px solid blue" }}>
-        App no : {appNo}
-        <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
-      </div>
-    </>
-  );
-}
-
+// 제이쿼리 다크모드 코드를 리액트 문법으로 바꾸기
 function App() {
-  AppCallCount++;
-  console.log(`App이 ${AppCallCount}번 실행!`);
-  const [no, setNo] = useState(0);
+  const [isDark, setIsDark] = useState(false);
+  // 기본상태는 false임.
+
+  //js문법을 적어야하는 경우 useEffect에 넣어주는 것이 좋다
+  useEffect(() => {
+    const html = document.getElementsByTagName("html")[0];
+    // document에서 tagname이 html인 el를 가져온다.
+    // 만약 isDark면 html의 classList에 dark를 추가하고 아니면, dark를 삭제한다.
+    // [isDark] -> isDark 값이 변할 때만 실행된다.
+    if (isDark) {
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+    }
+  }, [isDark]);
 
   return (
     <>
-      <div style={{ border: "10px solid red" }}>
-        <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
-        <hr />
-        <Sub appNo={no} />
-        {/* Sub에 App no 값 넘겨주기*/}
+      <div>
+        <button className="btn-toggle-theme" onClick={() => setIsDark(!isDark)}>
+          테마토글
+        </button>
       </div>
+      {/* 클릭하면 setIsDark는 isDark가 아니다.  */}
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        tempore similique quaerat, rerum sunt alias repellat aliquid! Nesciunt
+        fugit maiores quia obcaecati sed! A veniam eos earum porro eaque
+        commodi?
+      </div>
+
+      <h1 className="color-primary">하하 호호</h1>
     </>
   );
 }
