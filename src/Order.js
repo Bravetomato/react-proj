9f34719ef618af7b86bbbe699c719c01e1465162
Bravetomato <princeps2nd@gmail.com>
2023-01-03 22:09:30 +0900

    Order 컴포넌트 분리

--- src/Order.js
+++ src/Order.js
@@ -3,16 +3,59 @@ import React, { useState, useMemo } from "react";
 import "./App.css";
 
 // 음식 주문하기.
+function OrderMainFood({setMainFoodCount, mainFoodCount}) {
+  return(
+  <>
+  <h2>메인 ( 수량 : {mainFoodCount} )</h2>
+    <div>
+      <button onClick={() => setMainFoodCount(mainFoodCount + 1)}>증가</button>
+      <button onClick={() => setMainFoodCount(mainFoodCount == 1 ? 1 : mainFoodCount-1)}>감소</button>
+      {/* mainFoodCount-1 로 한 이유: 최소 수량을 1로 하기 위해 */}
+    </div>
+    </>
+  );
+}
+
+function OrderOptions({selectedCount, options, toggleAllChecked, btnAllChecked, optionCheckeds, toggleOptionCheck}) {
+  return(
+    <>
+     {/* selectedCount 에는 선택된 옵션 개수 / options.length 옵션 총 개수 */}
+     <hr />
+    <h2>옵션 ({selectedCount} / {options.length})</h2>
+     {/* optionCheckeds 의 모든(every) el를 살펴 본후 삼항연산자를 통해 참이면 체크, 거짓이면 빈칸  */}
+    <span style={{userSelect: "none", cursor: "pointer"}} 
+          onClick={toggleAllChecked}
+          >{btnAllChecked ? "[ v ]" : "[  ]"}전체선택</span>
+    <ul>
+      {/* map을 이용해 option 선택 하기. true에 v 표시가 생긴다. */}
+      {/* onClick={() => toggleOptionCheck(index)  클릭시 toggleOptionCheck(index) 실행*/}
+      {/* usetSelect: "none" 글자 드래그 방지, cursor: "pointer 마우스 포인터 손가락 모양으로*/}
+      {options.map((option, index)=> (
+        <li key = {option} style={{paddingLeft:15, usetSelect: "none", cursor: "pointer"}}
+         onClick={() => toggleOptionCheck(index)}
+         >
+      {/* optionCheckeds[index] 가 true면 체크, false면 빈칸 */}
+        {optionCheckeds[index] ? "[ v ]" : "[  ]" }
+        {option}
+        </li>
+      ))}
+    </ul>
+    </>  
+  );
+}
+
 function Order() {
   // 메인의 수량
   const [mainFoodCount, setMainFoodCount] = useState(1);
 
   const options = [
-    "콜라 1.5", 
+    "발사믹", 
     "머스타드 소스",
     "홀스래디쉬 소스",
     "스윗어니언 소스",
     "후추",
+    "올리브유",
+    "스테이크 소스",
   ];
 
   // option 선택 체크를 위해 체크는 true, 빈칸은 false
@@ -52,32 +95,14 @@ const toggleOptionCheck = (index) => {
   return (
     <>
     <h1>음식 주문</h1>
-    <h2>메인 ( 수량 : {mainFoodCount} )</h2>
-    <div>
-      <button onClick={() => setMainFoodCount(mainFoodCount + 1)}>증가</button>
-      <button onClick={() => setMainFoodCount(mainFoodCount == 1 ? 1 : mainFoodCount-1)}>감소</button>
-    </div>
-     {/* selectedCount 에는 선택된 옵션 개수 / options.length 옵션 총 개수 */}
-     <hr />
-    <h2>옵션 ({selectedCount} / {options.length})</h2>
-     {/* optionCheckeds 의 모든(every) el를 살펴 본후 삼항연산자를 통해 참이면 체크, 거짓이면 빈칸  */}
-    <span style={{userSelect: "none", cursor: "pointer"}} 
-          onClick={toggleAllChecked}
-          >{btnAllChecked ? "[ v ]" : "[  ]"}전체선택</span>
-    <ul>
-      {/* map을 이용해 option 선택 하기. true에 v 표시가 생긴다. */}
-      {/* onClick={() => toggleOptionCheck(index)  클릭시 toggleOptionCheck(index) 실행*/}
-      {/* usetSelect: "none" 글자 드래그 방지, cursor: "pointer 마우스 포인터 손가락 모양으로*/}
-      {options.map((option, index)=> (
-        <li key = {option} style={{paddingLeft:15, usetSelect: "none", cursor: "pointer"}}
-         onClick={() => toggleOptionCheck(index)}
-         >
-      {/* optionCheckeds[index] 가 true면 체크, false면 빈칸 */}
-        {optionCheckeds[index] ? "[ v ]" : "[  ]" }
-        {option}
-        </li>
-      ))}
-    </ul>
+    <OrderMainFood 
+      setMainFoodCount={setMainFoodCount}
+      mainFoodCount={mainFoodCount}
+      />
+    <OrderOptions 
+      selectedCount={selectedCount} options={options} toggleAllChecked={toggleAllChecked} 
+      btnAllChecked={btnAllChecked} optionCheckeds={optionCheckeds} toggleOptionCheck={toggleOptionCheck}
+    />
     </>
   );
 }
