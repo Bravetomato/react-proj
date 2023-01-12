8dcea40021ab5695b659b1e211df4461bd69d9f8
Bravetomato <princeps2nd@gmail.com>
2023-01-12 20:18:13 +0900

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React, { useState } from "react";
-import Order from "./Order";
+
+import  "./App.css";
 
 function App() {
+  const fruits = ["사과", "배", "바나나"];
+
+  const [selecteds, setSelecteds] = useState(new Array(fruits.length).fill(true));
+  // new Array를 fruits의 길이만큼 true로 채우겠다. 
+
+  const toggleFruitSeleced = (index) => {
+    const newSelecteds = selecteds.map((el, _index) => _index == index ? !el : el);
+    setSelecteds(newSelecteds);
+  };
+  // 선택 상태가 true 인지 false 인지
+  const selectedFruits = selecteds.map((el, index) => (el ? fruits[index] : el)).filter((el)=> el);
+  // 선택된 과일의 이름이 무엇인지
 
   return (
     <>
-    <Order/>
+    <ul>
+      {fruits.map((fruit, index) => (
+        <li kep={index}>
+          <lable>
+            <input checked={selecteds[index]} type="checkbox" onChange={() => toggleFruitSeleced(index)}/> 
+            {fruit}
+          </lable>
+        </li>
+      ))}
+    </ul>
+    <hr />
+    <div>
+      선택상태 : {selecteds.join(",")}
+      <hr />
+      선택된 과일 : {selectedFruits.join(",")}
+    </div>
     </>
   );
 }
