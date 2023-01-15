72cd341037e726650e733ae1a7702979f010927a
Bravetomato <princeps2nd@gmail.com>
2023-01-15 15:11:24 +0900

    crud-삭제,수정

--- src/App.js
+++ src/App.js
@@ -2,33 +2,54 @@ import React, { useState } from "react";
 
 import "./App.css";
 
-// crud : create. 추가
+// crud : 삭제, 수정
 function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (newTodo) => {
     const newTodos = [...todos, todos.length + 1];
     setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
   }
-  // newTodo는 배열값에 +1 씩 증가하고, 이 값을 setTodos가 받는다.
-  // 이를 addTodo라는 변수에 넣는다. 
+  // 할일 삭제
+  // filter를 통해 선택된 인덱스와 기존 인덱스가 다르다면 남긴다.
+  // 즉 선택된 인덱스는 삭제 된다.
+  
+  const modifyTodo = (index, newTodo) => {
+    const newTodos = todos.map((todos, _index) => 
+      _index != index ? todos : newTodo
+    ); 
+    setTodos(newTodos);
+  };
+  // 할일 수정(편집)
+  // map을 통해 할일을 보여준다. 
+  // 선택된 인덱스와 기존 인덱스가 같다면 새로운 할일을 보여준다
+  // 아니면 기존 할일을 보여준다. 
 
-  const onClick = () => {
-    // const newTodos = [...todos, todos.length + 1];
-    // setTodos(newTodos);
-
+  const onAddBtnClick = () => {
     addTodo(todos.length + 1);
-    //addTodo라는 변수를 만들어 todos의 길이에 +1 되도록.
+  };
+
+  const onRemoveBtnClick = () => {
+    removeTodo(1);
+  };
+
+  const onEditBtnClick = () => {
+    modifyTodo(1, "Hello");
   }
 
   return(
   <>
-  <button onClick={onClick}>{JSON.stringify(todos)}</button>
-  {/* click 시에 배열에 숫자가 추가된다. */}
-  {/* stringify : js 문법을 문자열로 반환해준다. ()안에는 반환할 값을 넣어준다. 숫자, 배열, 문자 등을 넣을 수 있다.  */}
+   <div>{JSON.stringjfy(todos)}</div>
+   <button onClick={onAddBtnClick}>Add</button>
+   <button onClick={onRemoveBtnClick}>Remove</button>
+   <button onClick={onEditBtnClick}>Edit</button>
   </>
   );
 }
 
 export default App;
-
