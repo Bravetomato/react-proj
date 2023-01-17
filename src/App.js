025d54bcac113db574d7c065f32a807c5dea1627
Bravetomato <princeps2nd@gmail.com>
2023-01-17 20:35:06 +0900

    상위, 하위컨포넌트 나누기

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React, { useState, useRef } from "react";
 
 import "./App.css";
 
-// crud : update. 수정
+// 상위, 하위 컴포넌트로 나누기
+function TodoApp({onBtnAddTodoClick, onBtnRemoveTodoClick, onBtnModifyTodoClick, todos}) {
+
+  return(
+  <>
+   <button onClick={onBtnAddTodoClick}>Add</button>
+   <button onClick={onBtnRemoveTodoClick}>Remove</button>
+   <button onClick={onBtnModifyTodoClick}>Modify</button>
+
+   <hr />
+   <ul>
+     {todos.map((todo, index) => (
+      <li key={index}>
+        {todo.id} {todo.content} {todo.regDate}
+      </li>
+     ))}
+   </ul>
+  </>
+  );
+}
+
 function App() {
   const [todos, setTodos] = useState([]);
   const lastTodoIdRef = useRef(0);
 
   const addTodo = (newContent) => {
     const id = ++lastTodoIdRef.current;
-  // id 값 1씩 증가시키기
 
     const newTodo = {
       id,
-  //  id 값 1씩 증가시키기 위해 비워둠. 
       content: newContent,
       regDate: "2023-01-17 12:12:12"
     };
@@ -22,15 +40,13 @@ function App() {
     setTodos(newTodos);
   }; 
 
-  const removeTodo = (index) => {
-    const newTodos = todos.filter((_, _index) => _index != index);
-    // todos의 index를 확인하여 기존 index와 선택된 index가 다를 때 거른다. 
-    // 다른 index 삭제, 같은 index 남김. 
+  const modifyTodo = (index, newContent) => {
+    const newTodos = todos.map((todo, _index) => _index != index ? todos : {...todo, content: newContent });
     setTodos(newTodos);
   };
 
-  const modifyTodo = (index, newContent) => {
-    const newTodos = todos.map((todo, _index) => _index != index ? todos : {...todo, content: newContent});
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
     setTodos(newTodos);
   };
 
@@ -48,18 +64,9 @@ function App() {
  
   return(
   <>
-   <button onClick={onBtnAddTodoClick}>Add</button>
-   <button onClick={onBtnRemoveTodoClick}>Remove</button>
-   <button onClick={onBtnModifyTodoClick}>Modify</button>
-
-   <hr />
-   <ul>
-     {todos.map((todo, index) => (
-      <li key={index}>
-        {todo.id} {todo.content} {todo.regDate}
-      </li>
-     ))}
-   </ul>
+  <TodoApp
+  onBtnAddTodoClick={onBtnAddTodoClick} onBtnRemoveTodoClick={onBtnRemoveTodoClick}
+  onBtnModifyTodoClick={onBtnModifyTodoClick} todos={todos} />
   </>
   );
 }
