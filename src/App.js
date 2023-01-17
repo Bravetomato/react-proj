6bf9aab6448c778344de2ede0687cfdf82f4cc86
Bravetomato <princeps2nd@gmail.com>
2023-01-17 19:43:14 +0900

    crud-useRef로 id +1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import "./App.css";
 
 // crud 
-// 객체를 추가하기
+// 객체를 추가하기-useRef 로 id +1 하기
 function App() {
   const [todos, setTodos] = useState([]);
-  const [lastTodoId, setLastTodoId] = useState(0);
+  const lastTodoIdRef = useRef(0);
 
   const addTodo = (newContent) => {
-    const id = lastTodoId + 1;
-    setLastTodoId(id);
+    const id = ++lastTodoIdRef.current;
   // id 값 1씩 증가시키기
-  // addTodo는 newContent에 Hello를 받는다. 
-  // id 값을 1씩 증가시키기 위해 useState로 lastTodoId, setLastTodoId 를 선언하고,
-  // const id 를 만들어 setLastTodoId() 에 id를 받는다.
 
     const newTodo = {
       id,
@@ -22,16 +18,13 @@ function App() {
       content: newContent,
       regDate: "2023-01-17 12:12:12"
     };
-  // newTodo라는 객체를 만들고, 안에 id, content, regDate 값을 넣었다. 
 
     const newTodos = [...todos, newTodo];
-    // ...todos : 이미 보여진 할일, newTodo : 새로운 할일.
     setTodos(newTodos);
   };
 
     const onBtnAddTodoClick = () => {
       addTodo("Hello");
-      // AddTodo는 Hello를 받는데, AddTodo를 선언해줘야함.
     };
  
   return(
