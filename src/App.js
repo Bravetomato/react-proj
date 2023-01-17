3775cbe3658f2943cbe1ecf616bf43970b58a815
Bravetomato <princeps2nd@gmail.com>
2023-01-17 21:25:56 +0900

    할일 입력 form 만들기

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React, { useState, useRef } from "react";
 
 import "./App.css";
 
-// 자식이 커스텀상태 구조 분해하지 않고 바로 사용
+// 할일 입력 form 만들기 
+function NewTodoForm({todosState}) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    // 입력값 양 옆 여백 동일하게 주기.
+    form.content.value = form.content.value.trim();
+
+    // 입력값이 없을 때 경고창 띄우기.
+    if( form.content.value.length === 0) {
+      alert("write your todo");
+      form.content.focus();
+      // 커서 위치
+      return;
+    }
+
+    // 입력값이 있다면 
+    // 입력 후 '' 즉 빈칸으로 만들어 주고
+    // 커서 위치
+    todosState.addTodo(form.content.value);
+    form.content.value = '';
+    form.content.focus();
+  }
+
+  return(
+  <form onSubmit={onSubmit}>
+    <input name="content" type="text" placeholder="write your todo" autoComplete="off" />
+    <input type="submit" value="Add" />
+    <input type="reset" value="delete" />
+    {/* type="submit" : button 과 같다 */}
+    {/* type="reset" : 취소 버튼 */}
+  </form>
+ );
+}
+
 function TodoApp({todosState}) {
-  const onBtnAddTodoClick = () => {
-    todosState.addTodo("Hello");
-  };
-
-  const onBtnRemoveTodoClick = () => {
-    todosState.removeTodo(1);
-  };
-
-  const onBtnModifyTodoClick = () => {
-    todosState.modifyTodo(1, "lol");
-  };
   return(
   <>
-   <button onClick={onBtnAddTodoClick}>Add</button>
-   <button onClick={onBtnRemoveTodoClick}>Remove</button>
-   <button onClick={onBtnModifyTodoClick}>Modify</button>
-
+  {/* NewTodoForm 을 받아준다. */}
+  <NewTodoForm todosState={todosState} />
    <hr />
    <ul>
      {todosState.todos.map((todo, index) => (
@@ -33,7 +56,6 @@ function TodoApp({todosState}) {
   );
 }
 
-// 커스텀 훅
 function useTodosState() {
   const [todos, setTodos] = useState([]);
   const lastTodoIdRef = useRef(0);
@@ -52,21 +74,19 @@ function useTodosState() {
   }; 
 
   const modifyTodo = (index, newContent) => {
-    const newTodos = todos.map((todo, _index) => _index != index ? todos : {...todo, content: newContent });
+    const newTodos = todos.map((todo, _index) => _index !== index ? todos : {...todo, content: newContent });
     setTodos(newTodos);
   };
 
   const removeTodo = (index) => {
-    const newTodos = todos.filter((_, _index) => _index != index);
+    const newTodos = todos.filter((_, _index) => _index !== index);
     setTodos(newTodos);
   };
 
-  // return 으로 addTodo, removeTodo, modifyTodo, todos 받음.
   return {
     addTodo, removeTodo, modifyTodo, todos
   }
 }
-
 
 function App() {
   const todosState = useTodosState();
