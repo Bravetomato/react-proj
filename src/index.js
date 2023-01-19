7d5bedc0c8c683a3d4ec2ac882a792437be9cb57
Bravetomato <princeps2nd@gmail.com>
2023-01-19 20:03:50 +0900

    MUI 설치, 실행

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from "./App.js";
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 
+// mui 사용하기
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // Create a theme instance.
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["GmarketSansMedium"]
+    },
+    palette: {
+      primary: {
+        main: "#ff8686",
+        contrastText: "#ffffff"
+      }
+    }
+  });
+  root.render (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* css 노멀라이즈 */}
+      <App />
+    </ThemeProvider>
+  );
