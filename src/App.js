c8eafae86fd26279b24f0b48483084dab528d830
Bravetomato <princeps2nd@gmail.com>
2023-01-19 21:50:39 +0900

    할일추가 꾸미기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from "react";
-import { Button, AppBar, Toolbar, TextField, ThemeProvider, CssBaseline, createTheme } from '@mui/material/';
+import React, { useState, useRef, useEffect } from "react";
+import { Button, AppBar, Toolbar, TextField, ThemeProvider, CssBaseline, createTheme, Chip, Box } from '@mui/material/';
 
 function useTodosState() {
   const [todos, setTodos] = useState([]);
@@ -14,8 +14,9 @@ function useTodosState() {
       regDate: dateToStr(new Date())
     };
 
-    const newTodos = [...todos, newTodo];
-    setTodos(newTodos);
+    // setTodos((todos) => [...todos, newTodo]);
+    setTodos((todos) => [newTodo, ...todos]);
+    // 최신글부터 보이도록
   };
 
   const modifyTodo = (index, newContent) => {
@@ -40,6 +41,14 @@ function useTodosState() {
 
 function App() {
   const todosState = useTodosState();
+
+  useEffect(() => {
+    todosState.addTodo("운동 데드리프트 유산소 스트레칭");
+    todosState.addTodo("요리");
+    todosState.addTodo("공부");
+  }, []);
+  // useEffect 사용해 addTodo에 운동, 요리, 공부를 추가함.
+  // [] : 빈배열을 넣어 운동, 요리, 공부가 한번씩만 나타나도록. 빈배열 쓰지않으면 무한대로 나타남.
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -73,6 +82,11 @@ function App() {
       <form onSubmit={onSubmit} className="flex flex-col mt-4 px-4 gap-2">
         {/* mt-4 : margin 4. px-4 : padding 4 */}
       <TextField
+        minRows={3}
+        maxRows={10}
+        multiline
+        // multiline : enter 입력시 입력창 줄바꿈
+        // minRows : 최소 줄. maxRows : 최대 줄. {}안에 숫자를 넣어준다.
         autoComplete="off"
         name="content"
         type="text"
@@ -80,19 +94,27 @@ function App() {
         // label : placeholder의 역할
         variant="outlined"
       />
-      <Button variant="contained">Add</Button>
+      <Button type="submit" variant="contained">Add</Button>
       </form>
       <div className="mt-4 px-4">
         <ul>
           {todosState.todos.map((todo) => (
             <li key={todo.id} className="mt-10">
               <div className="flex gap-2">
-                <span>Number : {todo.id}</span>
-                <span>Date : {todo.regDate}</span>
+                <Chip label={`Number : ${todo.id}`} className="!pt-1"variant="outlined" />
+                {/* ! : 앞에 느낌표를 사용하면 우선순위로 적용된다.*/}
+                <Chip label={todo.regDate} variant="outlined" className="!pt-1" color="primary" />
               </div>
-              <div>
-                {todo.content}
-              </div>
+              <div className="mt-a p-10 shadow rounded-[20px]" whitespace-pre-wrap leading-normal>
+              <Box
+                sx={{
+                width: 300,
+                height: 300,
+                backgroundColor: 'primary.dark',
+                }} 
+              > {todo.content} </Box></div>
+              {/* whitespace-pre-wrap : 자동 줄바꿈. 공백을 코드에 있는 그대로 표시.*/}
+              {/* leading-normal : 행간 정렬*/}
             </li>
           ))}
         </ul>
