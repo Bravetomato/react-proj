068dd541e7c8767d4b001e7427b092f602d8b199
Bravetomato <princeps2nd@gmail.com>
2023-01-22 13:39:55 +0900

    new Date 적용

--- src/App.js
+++ src/App.js
@@ -1,54 +1,121 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import "./App.css";
 
-// crud : 삭제, 수정
-function App() {
-  const [todos, setTodos] = useState([]);
+// 날짜 입력 기능 추가
+function NewTodoForm({todosState}) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    
+    const form = e.target;
+    
+    form.content.value = form.content.value.trim();
+    
+    if ( form.content.value.length == 0 ) {
+      alert('Write your todo!');
+      form.content.focus();
+      
+      return;
+    }
+    
+    todosState.addTodo(form.content.value);
+    form.content.value = '';
+    form.content.focus();
+  }
+  
+  return (
+    <form onSubmit={onSubmit}>
+      <input autoComplete="off" name="content" type="text" placeholder="Write your todo" />
+      <input type="submit" value="Add" />
+      <input type="reset" value="Reset" />
+    </form>
+  );
+}
 
-  const addTodo = (newTodo) => {
-    const newTodos = [...todos, todos.length + 1];
+function TodoApp({ todosState }) {
+  return (
+    <>
+      <NewTodoForm todosState={todosState} />
+      <hr />
+      <ul>
+        {todosState.todos.map((todo, index) => (
+          <li key={index}>
+            {todo.id} {dateToStr(todo.regDate)} {todo.content}
+            {/* 유틸리티 dateToStr 의 todo에서 regDate 값을 보여줌*/}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function useTodosState() {
+  const [todos, setTodos] = useState([]);
+  const lastTodoIdRef = useRef(0);
+
+  const addTodo = (newContent) => {
+    const id = ++lastTodoIdRef.current;
+
+    const newTodo = {
+      id,
+      content: newContent,
+      regDate: new Date()
+      // new Date() : 현재 날짜, 시간 표시를 해주는 함수.
+    };
+
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  };
+
+  const modifyTodo = (index, newContent) => {
+    const newTodos = todos.map((todo, _index) =>
+      _index != index ? todo : { ...todo, content: newContent }
+    );
     setTodos(newTodos);
   };
 
   const removeTodo = (index) => {
     const newTodos = todos.filter((_, _index) => _index != index);
     setTodos(newTodos);
+  };
+
+  return {
+    todos,
+    addTodo,
+    modifyTodo,
+    removeTodo
+  };
+}
+
+function App() {
+  const todosState = useTodosState();
+
+  return (
+    <>
+      <TodoApp todosState={todosState} />
+    </>
+  );
+}
+
+// 유틸리티
+// 날짜 객체 입력받아서 문장(yyyy-mm-dd hh:mm:ss)으로 반환한다.
+function dateToStr(d) {
+  const pad = (n) => {
+    return n < 10 ? "0" + n : n;
   }
-  // 할일 삭제
-  // filter를 통해 선택된 인덱스와 기존 인덱스가 다르다면 남긴다.
-  // 즉 선택된 인덱스는 삭제 된다.
-  
-  const modifyTodo = (index, newTodo) => {
-    const newTodos = todos.map((todos, _index) => 
-      _index != index ? todos : newTodo
-    ); 
-    setTodos(newTodos);
-  };
-  // 할일 수정(편집)
-  // map을 통해 할일을 보여준다. 
-  // 선택된 인덱스와 기존 인덱스가 같다면 새로운 할일을 보여준다
-  // 아니면 기존 할일을 보여준다. 
 
-  const onAddBtnClick = () => {
-    addTodo(todos.length + 1);
-  };
-
-  const onRemoveBtnClick = () => {
-    removeTodo(1);
-  };
-
-  const onEditBtnClick = () => {
-    modifyTodo(1, "Hello");
-  }
-
-  return(
-  <>
-   <div>{JSON.stringjfy(todos)}</div>
-   <button onClick={onAddBtnClick}>Add</button>
-   <button onClick={onRemoveBtnClick}>Remove</button>
-   <button onClick={onEditBtnClick}>Edit</button>
-  </>
+  return (
+    d.getFullYear() +
+    "-" +
+    pad(d.getMonth() + 1) +
+    "-" +
+    pad(d.getDate()) +
+    " " +
+    pad(d.getHours()) +
+    ":" +
+    pad(d.getMinutes()) +
+    ":" +
+    pad(d.getSeconds())
   );
 }
 
