e221719ae08fb09040c095141a633a81e0c083ae
Bravetomato <princeps2nd@gmail.com>
2023-02-12 21:25:05 +0900

    RouterEx-page 이동 히스토리남기기

--- src/RouterEx.js
+++ src/RouterEx.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button, } from "@mui/material";
 
-// url 변화에 따라 page가 변화하도록-디자인 추가
+// page 이동기록 히스토리
 
 function HomePage() {
     return(
@@ -27,28 +27,51 @@ function LoginPage() {
  );
 }
 
+function useHistory() {
+    const initialUrl = "home";
+    // home 을 initialUrl 변수에 넣어주면, home 대신 initialUrl 을 사용하면 된다. 
+    const [ url, setUrl ] = useState(initialUrl);
+    const [ historyUrls, setHistoryUrls ] = useState([initialUrl]);
+    // const [ historyUrls, setHistoryUrls ] 의 경우, 기록이 여러개 남기 때문에 [] 배열에 initialUrl을 담아준다. 
+
+    // 클릭하면 movePage의 seturl이 home으로 변화하고, setHistoryUrls에 적용됨.
+    const movePage = (url) => {
+        setUrl(url);
+        setHistoryUrls([url, ...historyUrls]);
+        // 방금 클릭한 url 먼저 나오고, 기존 기록 url들이 나타남.
+    };
+
+    return {
+        url,
+        movePage,
+        historyUrls,
+    };
+}
+
 export default function RouterEx() {
-    const [ url, setUrl ] = useState("home");
-    // default 는 home 이기 때문에 처음에는 Homepage가 나타남. 
+    const history = useHistory();
+
     return(
         <>
         {/* 현재 url을 표시하기 위해 */}
-        <div className="p-5">현재 주소 : {url}</div>
+        <div className="p-5">현재 주소 : {history.url}</div>
+        <div className="p-5">히스토리 : {history.historyUrls.join(",")}</div>
          <ul className="flex gap-3 p-5">
-            <li onClick={() => setUrl("home")} className="hover:text-red-300 cursor-pointer">
+            {/* 클릭시 history 에 있는 movePage 를 home 으로 바꿔준다.  */}
+            <li onClick={() => history.movePage("home")} className="hover:text-red-300 cursor-pointer">
                 <Button variant="outlined">Home</Button>
             </li>
-            <li onClick={() => setUrl("about")} className="hover:text-red-300 cursor-pointer">
+            <li onClick={() => history.movePage("about")} className="hover:text-red-300 cursor-pointer">
                 <Button variant="outlined">About</Button>
             </li>
-            <li onClick={() => setUrl("login")} className="hover:text-red-300 cursor-pointer">
+            <li onClick={() => history.movePage("login")} className="hover:text-red-300 cursor-pointer">
                 <Button variant="outlined">Login</Button>
             </li>
          </ul>
          <div className="p-5">
-         {url == "home" && <HomePage />}
-         {url == "about" && <AboutPage />}
-         {url == "login" && <LoginPage />}
+         {history.url == "home" && <HomePage />}
+         {history.url == "about" && <AboutPage />}
+         {history.url == "login" && <LoginPage />}
          </div>
         </>
  );
