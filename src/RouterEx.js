6ac928666e831c78176ee973f4a1bf7836de4804
Bravetomato <princeps2nd@gmail.com>
2023-02-13 00:44:04 +0900

    react-router-dom isActive, classNaames 사용

--- src/RouterEx.js
+++ src/RouterEx.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Button, } from "@mui/material";
 import { Routes, Route, Navigate, useLocation, NavLink } from "react-router-dom";
+import classNames from "classnames";
 
-// Router 사용-NavLink
+// Router 사용-NavLink 의 isActive, classNames 사용.
 
 function HomeMainPage() {
     return(
@@ -29,8 +30,16 @@ export default function RouterEx() {
      {/* 현재 주소 location.pathname 으로 얻을 수 있다.  */}
      <header>현재 주소 : {location.pathname}
       <hr />
-      <NavLink to="/home/main" className="btn btn-link">MAIN</NavLink>
-      <NavLink to="/home/about" className="btn btn-link">ABOUT</NavLink>
+      <NavLink to="/home/main" className={({ isActive }) => 
+        classNames("btn", {"btn-link": !isActive }, {"btn-primary": isActive })}>
+        MAIN</NavLink>
+        {/* {"btn-link": !isActive } : 현재 페이지가 아닐때는 btn-link 를 쓰고
+            {"btn-primary": isActive } : 현재 페이지 일때에는  btn-primary 를 쓰겠다.*/}
+        {/* isActive =  false -> btn btn-link
+            isctive = true -> btn btn-primary */}
+      <NavLink to="/home/about" className={({ isActive }) => 
+        classNames("btn", {"btn-link": !isActive }, {"btn-primary": isActive })}>
+        ABOUT</NavLink>
      </header>
         <Routes>
             <Route path="/home/main" element={<HomeMainPage />} />
