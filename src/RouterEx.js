6a883c7f4f0c9f0d18ff73f3eba53c64c64ff366
Bravetomato <princeps2nd@gmail.com>
2023-02-13 00:18:26 +0900

    react-router-dom location.pathname

--- src/RouterEx.js
+++ src/RouterEx.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Button, } from "@mui/material";
-import { Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
-// Router 사용하기.
+// Router 사용-현재 주소에 접근하기.
 
 function HomeMainPage() {
     return(
@@ -20,23 +20,18 @@ function HomeAboutPage() {
  );
 }
 
-function UserLoginPage() {
-    return(
-    <>
-     <h1>User Login</h1>
-    </>
- );
-}
-
 export default function RouterEx() {
+    const location = useLocation();
     // 이 자리는 기본 url이 들어간다. 
     return(
      <>
+     {/* react router site 참고 */}
+     {/* 현재 주소 location.pathname 으로 얻을 수 있다.  */}
+     <header>현재 주소 : {location.pathname}</header>
         <Routes>
             <Route path="/home/main" element={<HomeMainPage />} />
             <Route path="/home/about" element={<HomeAboutPage />} />
-            <Route path="/user/login" element={<UserLoginPage />} />
-            <Route path="*" element={<Navigate to="/user/login" />} />
+            <Route path="*" element={<Navigate to="/home/main" />} />
             {/* "*" : 모든 url을 포함. 즉 해당 url 없을 때  "/user/login" 로 Navigate to , 향한다.*/}
         </Routes>
      </>
